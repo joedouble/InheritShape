1cc1ab42c80b0ae03994bc378b7872228b2ac921
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-02-04 09:38:08 -0500

    Submitting latest code.

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Rectangle extends Shape {
+
+    private double width;
+    private double length;
+
+    public Rectangle(String color, boolean filled) {
+        super(color, filled);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+}
